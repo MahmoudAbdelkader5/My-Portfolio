95eb99092f5544d5b0ad76cc4faee069c0969f3f
mahmoud <mm4084@fayoum.edu.eg>
2025-03-13 10:53:46 +0200

    last update

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,  
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
